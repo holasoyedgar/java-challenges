36f72af0450187075c9a7c932390b065fc55f936
Edgar Rincon <eers222@gmail.com>
2026-03-10 12:12:29 -0600

    Remove unused import

--- src/main/java/com/example/challenges/PropertyMerger.java
+++ src/main/java/com/example/challenges/PropertyMerger.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class PropertyMerger {
