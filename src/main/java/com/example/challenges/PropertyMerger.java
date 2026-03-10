9fa696002339f20e473bc76011cbbbe88b8879aa
Edgar Rincon <eers222@gmail.com>
2026-03-09 20:42:45 -0600

    Final version of PropertyMerger

--- src/main/java/com/example/challenges/PropertyMerger.java
+++ src/main/java/com/example/challenges/PropertyMerger.java
@@ -1,66 +1,25 @@
 package com.example.challenges;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+import java.util.stream.Collectors;
 
 public class PropertyMerger {
 
-
     public List<MergedProperty> mergeListings(List<MlsListing> listings) {
-        List<MlsListing> orderedListings = listings.stream()
-                .sorted(Comparator.comparing(MlsListing::lastUpdated).reversed())
-                .toList();
-
-        Map<String, MergedProperty> mergedPropertyMap = createMergedPropertyMap(orderedListings);
-
-        return mergedPropertyMap
-                .values()
+        return listings.stream()
+                .collect(Collectors.toMap(
+                        MlsListing::zpid,
+                        PropertyMerger::createMergedProperty,
+                        PropertyMerger::updateMergedProperty
+                )).values()
                 .stream()
                 .sorted(Comparator.comparing(MergedProperty::zpid))
                 .toList();
     }
-
-
-    private static Map<String, MergedProperty> createMergedPropertyMap(List<MlsListing> orderedListings) {
-        Map<String, MergedProperty> mergedPropertyMap = new HashMap<>();
-        for (MlsListing listing : orderedListings) {
-            MergedProperty newMergedProperty;
-            if (mergedPropertyMap.containsKey(listing.zpid())) {
-                newMergedProperty = updateMergedProperty(mergedPropertyMap.get(listing.zpid()), listing);
-            } else {
-                newMergedProperty = createMergedProperty(listing);
-
-            }
-            mergedPropertyMap.put(listing.zpid(), newMergedProperty);
-        }
-        return mergedPropertyMap;
-    }
-
-
-    private static MergedProperty updateMergedProperty(MergedProperty exisingMergedProperty, MlsListing listing) {
-        Integer bedrooms = exisingMergedProperty.bedrooms();
-        if (bedrooms == null) {
-            bedrooms = listing.bedrooms();
-        }
-        Integer bathrooms = exisingMergedProperty.bathrooms();
-        if (bathrooms == null) {
-            bathrooms = listing.bathrooms();
-        }
-
-        return new MergedProperty(
-                exisingMergedProperty.zpid(),
-                exisingMergedProperty.lastUpdated(),
-                exisingMergedProperty.price(),
-                bedrooms,
-                bathrooms
-        );
-
-    }
-
 
     private static MergedProperty createMergedProperty(MlsListing listing) {
         return new MergedProperty(
@@ -72,9 +31,39 @@ public class PropertyMerger {
         );
     }
 
+    private static MergedProperty updateMergedProperty(MergedProperty existingMergedProperty, MergedProperty newProperty) {
+        BigDecimal price = existingMergedProperty.price();
+        LocalDateTime lastUpdated = existingMergedProperty.lastUpdated();
+        Integer bedrooms = existingMergedProperty.bedrooms();
+        Integer bathrooms = existingMergedProperty.bathrooms();
+
+        if (lastUpdated.isBefore(newProperty.lastUpdated())) {
+            lastUpdated = newProperty.lastUpdated();
+            price = newProperty.price();
+            if (newProperty.bedrooms() != null) {
+                bedrooms = newProperty.bedrooms();
+            }
+            if (newProperty.bathrooms() != null) {
+                bathrooms = newProperty.bathrooms();
+            }
+        } else {
+            if (bedrooms == null) {
+                bedrooms = newProperty.bedrooms();
+            }
+            if (bathrooms == null) {
+                bathrooms = newProperty.bathrooms();
+            }
+        }
+
+        return new MergedProperty(
+                existingMergedProperty.zpid(),
+                lastUpdated,
+                price,
+                bedrooms,
+                bathrooms
+        );
+    }
 }
 
-
-record MlsListing(String zpid, LocalDateTime lastUpdated, Double price, Integer bedrooms, Integer bathrooms) { }
-
-record MergedProperty(String zpid, LocalDateTime lastUpdated, Double price, Integer bedrooms, Integer bathrooms) { }
+record MlsListing(String zpid, LocalDateTime lastUpdated, BigDecimal price, Integer bedrooms, Integer bathrooms) {}
+record MergedProperty(String zpid, LocalDateTime lastUpdated, BigDecimal price, Integer bedrooms, Integer bathrooms) {}
