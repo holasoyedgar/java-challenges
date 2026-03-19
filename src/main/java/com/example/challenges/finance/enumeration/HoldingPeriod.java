7ed024671d2032e7c06938b5a58d39559c567075
Edgar Rincon <eers222@gmail.com>
2026-03-19 16:05:46 -0600

    Delete unused code

--- src/main/java/com/example/challenges/finance/enumeration/HoldingPeriod.java
+++ src/main/java/com/example/challenges/finance/enumeration/HoldingPeriod.java
@@ -1,18 +1,6 @@
 package com.example.challenges.finance.enumeration;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 public enum HoldingPeriod {
     LONG_TERM,
-    SHORT_TERM;
-
-    private static final Set<String> HOLDING_PERIODS = Arrays.stream(values())
-            .map(Enum::name)
-            .collect(Collectors.toUnmodifiableSet());
-
-    public static boolean isValid(String holdingPeriod) {
-        return holdingPeriod != null && HOLDING_PERIODS.contains(holdingPeriod);
-    }
+    SHORT_TERM
 }
